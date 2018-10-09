f35b75d4c35c93a386243f8a0b57a15ab0cb08df
chenqiankun <chenqiankun@sogou-inc.com>
2018-10-09 15:51:39 +0800

    add redis pool test

--- src/main/java/com/example/configuration/EntityManagerConfig.java
+++ src/main/java/com/example/configuration/EntityManagerConfig.java
@@ -1,9 +1,13 @@
 package com.example.configuration;
 
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class EntityManagerConfig {
@@ -12,5 +16,9 @@ public class EntityManagerConfig {
             EntityManagerFactoryBuilder builder){
 //        return builder.dataSource(dataSource);
         return null;
+    }
+    
+    public PlatformTransactionManager jpaTxManager(EntityManagerFactory factory){ // jpaTxManager
+        return new JpaTransactionManager(factory);
     }
 }
